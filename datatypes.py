814ca673fa92f16d3987442ab7c881f5efa32663
modugurakesh <rockymodugu79@gmail.com>
2025-07-17 13:12:35 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+
+frist_intger = int(input('Enter frist number:'))
+second_sting = input('Enter  String:')
+third_float = float(input('Enter third float:'))
+four = bool('Enter a Boolean Value: ')
+complex_num = ('Enter a Value: ')
+l = []
+dictonary = dict()
+sett = set()
+tuplee = tuple()
+
+# Print the type of each variable
+print('Type of frist_intger:', type(frist_intger))
+print('Type of second_sting:', type(second_sting))
+print('Type of third_float:', type(third_float))   
+print('Type of Boolean Value:', type(four))
+print(type(l))
+print(type(complex_num))
+print() 
+# Print the values of each variable
