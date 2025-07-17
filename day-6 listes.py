33335eccf5e34dd50fd2b8f3dbb52ecb961f1fad
modugurakesh <rockymodugu79@gmail.com>
2025-07-17 13:14:27 +0530

    Add files via upload

---
+++ day-6 listes.py
@@ -0,0 +1,24 @@
+# Day 6 - Lists in Python
+
+# Example: Creating and using lists
+
+# Creating a list of fruits
+fruits = ["apple", "banana", "cherry"]
+
+# Adding an item to the list
+fruits.append("orange")
+
+# Removing an item from the list
+fruits.remove("banana")
+
+# Accessing items by index
+first_fruit = fruits[0]
+
+# Looping through the list
+for fruit in fruits:
+    print(fruit)
+
+# Output the first fruit
+print("The first fruit is:", first_fruit)   
+sort(reverse=True
+)
