71c9e4be967f5d6b42d2e6cd4b983a4c275f8f0f
modugurakesh <rockymodugu79@gmail.com>
2025-07-17 13:11:52 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,18 @@
+# Additions -- 1
+
+a = int(input())
+b = int(input())
+print('Addition:',a+b)
+print('sub:',a-b)
+print('Multi:',a*b)
+print('division:',a/b)
+print('floor division:',a//b)
+print('modules:', a % b) 
+print('power:', a**b)  
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# List of Keywords 
+
+import keyword as rakesh
+print(rakesh.kwlist) 
