43e491705177886cf89633d598d0e18b3c4595d2
Rakesh Modugu <rakeshmodugu94@gmail.com>
2025-07-23 19:58:27 +0530

    Add files via upload

---
+++ day-7 tuple.py
@@ -0,0 +1,10 @@
+print('Hello world')
+
+a = tuple(map(int,input('Enter a input : ').split(',')))
+print(a)
+
+
+b = set(map(int,input('Enter b input : ').split(',')))
+print(b)
+c = set(map(int,input('Enter c input : ').split(',')))
+print(c)
